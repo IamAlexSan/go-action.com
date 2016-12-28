e0ec5085c36a3ef966de77461dac0113656ebbf7
Programer <dahnenko02501@gmail.com>
2016-12-28 05:35:48 +0200

    Changes

--- Layouts/js/main.js
+++ Layouts/js/main.js
@@ -1,11 +1,44 @@
-$('#ShowButton').click(function () {
+$('#visability').click(function () {
     var password = $('#password');
 
     if(password.attr('type') == 'password'){
+
         password.attr('type','text');
-        $('#ShowButton').text('Hide password');
+        $('#visability').text('Hide password');
+
+        $('#visability').attr('src','/Layouts/images/visible-hover.png');
+
     }else if(password.attr('type') == 'text'){
+
         password.attr('type','password');
-        $('#ShowButton').text('Show password');
+        $('#visability').text('Show password');
+
+        $('#visability').attr('src','/Layouts/images/visible.png');
+
     }
+});
+
+
+$("#recovery").hover(function(){
+    $("#Mail, #Login").css("border", "3px solid greenyellow");
+    $("#Mail, #Login").css("color", "greenyellow");
+}, function(){
+    $("#Mail, #Login").css("border-color", "grey");
+    $("#Mail, #Login").css("color", "black");
+});
+
+$("#SignIn").hover(function(){
+    $("#password, #Login").css("border", "3px solid #2aabd2");
+    $("#password, #Login").css("color", "#2aabd2");
+}, function(){
+    $("#password, #Login").css("border-color", "grey");
+    $("#password, #Login").css("color", "black");
+});
+
+$("#Join").hover(function(){
+    $("#password, #Login, #Mail").css("border", "3px solid #b92c28");
+    $("#password, #Login, #Mail").css("color", "#b92c28");
+}, function(){
+    $("#password, #Login, #Mail").css("border-color", "grey");
+    $("#password, #Login, #Mail").css("color", "black");
 });
